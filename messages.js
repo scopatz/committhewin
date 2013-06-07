bae8babe089b9f7f75d28f353c3c278460f1c776
Anthony Scopatz <scopatz@gmail.com>
2013-06-06 20:06:08 -0500

    initial commit

---
+++ messages.js
@@ -0,0 +1,22 @@
+var pytables = '<a href="http://pytables.github.io/">PyTables</a>';
+
+// Row elements have form:
+//   * comment
+//   * person or null
+//   * project or null
+//   * Date or null
+var msgs = [
+    ["Many thanks for keeping such a great piece of work up and running. \
+     I've just seen some features in the release notes, features which I \
+     was going to need in the very near future! \nGreat job!",
+     "Seref Arikan", pytables, new Date(2013, 6, 3, 0, 0, 0, 0)],
+    ["Thank you from a happy user :)))", "Julio Trevisan", pytables, 
+     new Date(2013, 6, 2, 0, 0, 0, 0)],
+    ["Thanks Anthony. You are super helpful as always! Thanks for all your hard work.", 
+     "{Private}", pytables, new Date(2013, 6, 2, 0, 0, 0, 0)], 
+    ];
+
+function getRandomMessage(){
+    var msg = msgs[Math.floor(Math.random()*msgs.length)];
+    return msg;
+};
---
+++ readme.rst
@@ -0,0 +1,9 @@
+Commit The Win
+==============
+This is a website that displays nice comments made about open source projects.
+
+**Usage:** Take as a little pick-me-up.  Reload as needed.
+
+**Contributing:** Feel free to add comments via pull request.
+
+**Thanks:** To http://whatthecommit.com/ for inspiration and code.
